beb115321b9ba5f16c969543d69c97147bf5b925
tmoret <thomas.moret@xfk48.com>
2021-12-02 18:02:20 +0100

    42 Lausanne

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmoret <tmoret@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 10:10:47 by tmoret            #+#    #+#             */
-/*   Updated: 2021/12/01 22:58:48 by tmoret           ###   ########.fr       */
+/*   Updated: 2021/12/02 17:53:38 by tmoret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,51 @@
 void	ft_hello(void)
 {
 	ft_printf("\n");
-	printf("Hello World!\n");
-	ft_printf("Hello World!\n");
+	printf("-%d\n", printf("Hello World!\n"));
+	ft_printf("-%d\n", ft_printf("Hello World!\n"));
 	ft_printf("\n");
-	printf("c---%c %c \n", '4', '8');
-	ft_printf("c---%c %c \n", '4', '8');
+	printf("-%d\n", printf("c |---| %c %c \n", '4', '8'));
+	ft_printf("-%d\n", ft_printf("c |---| %c %c \n", '4', '8'));
 	ft_printf("\n");
-	printf("c---%c %c %c\n", 'b', 'n', 'k');
-	ft_printf("c---%c %c %c\n", 'b', 'n', 'k');
+	printf("-%d\n", printf("c |---| %c %c %c\n", 'b', 'n', 'k'));
+	ft_printf("-%d\n", ft_printf("c |---| %c %c %c\n", 'b', 'n', 'k'));
 	ft_printf("\n");
-	printf("s---%s \n", "BNK");
-	ft_printf("s---%s \n", "BNK");
+	printf("-%d\n", printf("s |---| %s \n", "BNK"));
+	ft_printf("-%d\n", ft_printf("s |---| %s \n", "BNK"));
+	ft_printf("\n");
+}
+
+void	ft_pointeur(int *b)
+{
+	printf("-%d\n", printf("p |---| %p\n", b));
+	ft_printf("-%d\n", ft_printf("p |---| %p\n", b));
+	ft_printf("\n");
+}
+
+void	ft_decimal(int a)
+{
+	printf("-%d\n", printf("d |---| %d\n", a));
+	ft_printf("-%d\n", ft_printf("d |---| %d\n", a));
+	ft_printf("\n");
+	printf("-%d\n", printf("i |---| %i\n", a));
+	ft_printf("-%d\n", ft_printf("i |---| %i\n", a));
+	ft_printf("\n");
+	printf("-%d\n", printf("u |---| %u\n", a));
+	ft_printf("-%d\n", ft_printf("u |---| %u\n", a));
 	ft_printf("\n");
 }
 
 void	ft_xx(int a)
 {
-	printf("x---%x\n", a);
-	ft_printf("x---%x\n", a);
+	printf("-%d\n", printf("x |---| %x\n", a));
+	ft_printf("-%d\n", ft_printf("x |---| %x\n", a));
 	ft_printf("\n");
-	printf("X---%X\n", a);
-	ft_printf("X---%X\n", a);
+	printf("-%d\n", printf("X |---| %X\n", a));
+	ft_printf("-%d\n", ft_printf("X |---| %X\n", a));
 	ft_printf("\n");
-}
-
-void	ft_pourcent(void)
-{
-	printf("%% \n");
-	ft_printf("%% \n");
+	printf("-%d\n", printf("%% \n"));
+	ft_printf("-%d\n", ft_printf("%% \n"));
 	ft_printf("\n");
-}
-
-void	ft_count(int a)
-{
-	printf("-%d\n", printf("X---%X\n", a));
-	ft_printf("-%d\n", ft_printf("X---%X\n", a));
 }
 
 int	main(void)
@@ -61,19 +71,7 @@ int	main(void)
 	a = 48;
 	b = &a;
 	ft_hello();
-	printf("p---%p\n", b);
-	ft_printf("p---%p\n", b);
-	ft_printf("\n");
-	printf("d---%d\n", a);
-	ft_printf("d---%d\n", a);
-	ft_printf("\n");
-	printf("i---%i\n", a);
-	ft_printf("i---%i\n", a);
-	ft_printf("\n");
-	printf("u---%u\n", a);
-	ft_printf("u---%u\n", a);
-	ft_printf("\n");
+	ft_pointeur(b);
+	ft_decimal(a);
 	ft_xx(a);
-	ft_pourcent();
-	ft_count(a);
 }
