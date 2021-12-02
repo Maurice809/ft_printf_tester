27590582b1d3be9c61d91fc81a366709df146e91
tmoret <thomas.moret@xfk48.com>
2021-12-02 14:36:09 +0100

    42 Lausanne

--- eval.sh
+++ eval.sh
@@ -6,7 +6,7 @@
 #    By: tmoret <marvin@42lausanne.ch>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/02 09:57:40 by tmoret            #+#    #+#              #
-#    Updated: 2021/12/02 10:16:44 by tmoret           ###   ########.fr        #
+#    Updated: 2021/12/02 14:35:12 by tmoret           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,7 +25,7 @@ fi
 git clone $repo ft_printf
 norminette ./ft_printf
 sleep 10s
-git clone http://cherprang.xfk48.com:3000/42_Lausanne/ft_printf_tester.git
+git clone https://github.com/Maurice809/ft_printf_tester.git
 
 cd ft_printf_tester
 ./c.sh
